baaced2a39fd0ec1021f638fd8f4b0b6bcf6c7eb
Lygist <152767368+Lygist@users.noreply.github.com>
2024-06-22 18:27:48 -0400

    train.py

--- train.py
+++ train.py
@@ -1,9 +1,7 @@
 import numpy as np
-
 import matplotlib.pyplot as plt
 import seaborn as sns
 import models
-
 import torch
 from torch import autograd
 import torch.nn as nn
@@ -11,13 +9,11 @@ from torch.utils.data import DataLoader, dataset
 from torchvision.datasets import MNIST
 import torchvision.transforms as T
 eps = np.finfo(float).eps
-
 import tqdm
 from torchsummary import summary
 import argparse
 import os
 import time
-
 
 desc = "Pytorch implementation of AAE'"
 parser = argparse.ArgumentParser(description=desc)
@@ -29,12 +25,7 @@ parser.add_argument('--use_cuda', type=bool, default=False, help='Use GPU?')
 parser.add_argument('--log_interval', type=int, default=100)
 
 args = parser.parse_args()
-
 EPS = 1e-15
-
-
-""" GPU """
-# Enable CUDA, set tensor type and device
 
 use_cuda = True
 use_cuda = use_cuda and torch.cuda.is_available()
@@ -46,48 +37,38 @@ else:
     dtype = torch.FloatTensor
     device = torch.device("cpu")
 
-""" Directory loading """
-
 Model_dir = 'Models/'
-
 if not os.path.exists(Model_dir):
     os.makedirs(Model_dir)
 
 Data_dir = 'Data/'
-
 if not os.path.exists(Data_dir):
     os.makedirs(Data_dir)
 
 log_dir = 'logs/'
-
 if not os.path.exists(log_dir):
     os.makedirs(log_dir)
 
 Fig_dir = 'Figs/'
-
 if not os.path.exists(Fig_dir):
     os.makedirs(Fig_dir)
 
-
-
-""" Data loaders """
-
 train_loader = torch.utils.data.DataLoader(MNIST('Data/', train=True, download=True,
                              transform=T.Compose([
-                               T.transforms.ToTensor(),
+                               T.ToTensor(),
                                T.Normalize(
                                  (0.1307,), (0.3081,))
-                             ])),batch_size=args.batch_size, shuffle=True)
+                             ])), batch_size=args.batch_size, shuffle=True)
 
-val_loader = torch.utils.data.DataLoader(MNIST('Diles/', train=False, download=True,
+val_loader = torch.utils.data.DataLoader(MNIST('Data/', train=False, download=True,
                              transform=T.Compose([
-                               T.transforms.ToTensor(),
+                               T.ToTensor(),
                                T.Normalize(
                                  (0.1307,), (0.3081,))
-                             ])),batch_size=args.batch_size, shuffle=False)
-
+                             ])), batch_size=args.batch_size, shuffle=False)
 
 ae_criterion = nn.MSELoss()
+
 def train_validate(encoder, decoder, Disc, dataloader, optim_encoder, optim_decoder, optim_D, train):
     total_rec_loss = 0
     total_disc_loss = 0
@@ -102,13 +83,11 @@ def train_validate(encoder, decoder, Disc, dataloader, optim_encoder, optim_deco
         Disc.eval()
 
     for i, (data, labels) in enumerate(dataloader):
-        """ Reconstruction loss """
         for p in Disc.parameters():
             p.requires_grad = False
 
         real_data_v = autograd.Variable(data).to(device)
         real_data_v = real_data_v.view(-1, 784)
-        #print(real_data_v.size())
         encoding = encoder(real_data_v)
         fake = decoder(encoding)
         ae_loss = ae_criterion(fake, real_data_v)
@@ -120,7 +99,6 @@ def train_validate(encoder, decoder, Disc, dataloader, optim_encoder, optim_deco
             optim_encoder.step()
             optim_decoder.step()
 
-        """ Discriminator loss """
         encoder.eval()
         z_real_gauss = autograd.Variable(torch.randn(data.size()[0], args.dim_z) * 5.).to(device)
         D_real_gauss = Disc(z_real_gauss)
@@ -136,7 +114,6 @@ def train_validate(encoder, decoder, Disc, dataloader, optim_encoder, optim_deco
             D_loss.backward()
             optim_D.step()
 
-        """ Generator loss """
         if train:
             encoder.train()
         else:
@@ -152,26 +129,20 @@ def train_validate(encoder, decoder, Disc, dataloader, optim_encoder, optim_deco
             G_loss.backward()
             optim_encoder_reg.step()
 
-
         if i % 100 == 0:
-            print ('\n Step [%d], recon_loss: %.4f, discriminator_loss :%.4f , generator_loss:%.4f'
-                    %(i, ae_loss.item(), D_loss.item(), G_loss.item()))
+            print('\n Step [%d], recon_loss: %.4f, discriminator_loss :%.4f , generator_loss:%.4f' % (i, ae_loss.item(), D_loss.item(), G_loss.item()))
 
     M = len(dataloader.dataset)
-    return total_rec_loss /M  , total_disc_loss /M , total_gen_loss /M
+    return total_rec_loss / M, total_disc_loss / M, total_gen_loss / M
 
+encoder = models.Encoder(784, args.dim_z).to(device)
+decoder = models.Decoder(784, args.dim_z).to(device)
+Disc = models.Discriminator(args.dim_z, 500).to(device)
 
-encoder = models.Encoder(784,args.dim_z).to(device)
-decoder = models.Decoder(784,args.dim_z).to(device)
-Disc = models.Discriminator(args.dim_z,500).to(device)
-
-
-#encode/decode optimizers
 optim_encoder = torch.optim.Adam(encoder.parameters(), lr=args.lr)
 optim_decoder = torch.optim.Adam(decoder.parameters(), lr=args.lr)
 optim_D = torch.optim.Adam(Disc.parameters(), lr=args.lr)
 optim_encoder_reg = torch.optim.Adam(encoder.parameters(), lr=0.0001)
-
 
 schedulerDisc = torch.optim.lr_scheduler.ExponentialLR(optim_D, gamma=0.99)
 schedulerD = torch.optim.lr_scheduler.ExponentialLR(optim_decoder, gamma=0.99)
@@ -180,14 +151,14 @@ schedulerE = torch.optim.lr_scheduler.ExponentialLR(optim_encoder, gamma=0.99)
 train_loss = []
 val_loss = []
 for epoch in range(args.epochs):
-    l1,l2 ,l3 = train_validate(encoder, decoder, Disc, train_loader, optim_encoder, optim_decoder, optim_D, True)
+    l1, l2, l3 = train_validate(encoder, decoder, Disc, train_loader, optim_encoder, optim_decoder, optim_D, True)
     print('\n epoch:{} ---- training loss:{}'.format(epoch, l1))
     train_loss.append(l1)
 
     if epoch % 5 == 0:
         l1, l2, l3 = train_validate(encoder, decoder, Disc, val_loader, optim_encoder, optim_decoder, optim_D, False)
         print('\n epoch:{} ---- validation loss loss:{}'.format(epoch, l1))
-        val_loss.append(l1 )
+        val_loss.append(l1)
 
 sns.set()
 plt.rcParams['figure.figsize'] = 5, 5
@@ -199,9 +170,10 @@ plt.ylabel('Reconstruction loss')
 plt.legend()
 plt.grid(True)
 plt.savefig(Fig_dir + 'training_' + str(args.epochs) + 'epochs.png')
-#save the Encoder
-torch.save(encoder.state_dict(),Model_dir + 'encoder_z'+ str(args.dim_z)+'_epch'+str(args.epochs)+'.pt')
-torch.save(decoder.state_dict(),Model_dir +'decoder_z'+ str(args.dim_z)+'_epch'+str(args.epochs)+'.pt')
-torch.save(Disc.state_dict(),Model_dir +'disc_z'+ str(args.dim_z)+'_epch'+str(args.epochs)+'.pt')
+
+torch.save(encoder.state_dict(), Model_dir + f'encoder_z{args.dim_z}_epch{args.epochs}.pt')
+torch.save(decoder.state_dict(), Model_dir + f'decoder_z{args.dim_z}_epch{args.epochs}.pt')
+torch.save(Disc.state_dict(), Model_dir + f'disc_z{args.dim_z}_epch{args.epochs}.pt')
+
 
 
